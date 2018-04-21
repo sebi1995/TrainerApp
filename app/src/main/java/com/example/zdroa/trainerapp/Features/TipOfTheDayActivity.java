22d65c9eee3d7edbe7590731d2766e90cf2701ce
Sebastian M Zdroana <s.zdroana@gmail.com>
2018-04-21 09:39:26 +0300

    .

---
+++ app/src/main/java/com/example/zdroa/trainerapp/Features/TipOfTheDayActivity.java
@@ -0,0 +1,15 @@
+package com.example.zdroa.trainerapp.Features;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.zdroa.trainerapp.R;
+
+public class TipOfTheDayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_of_the_day);
+    }
+}
---
+++ app/src/main/java/com/example/zdroa/trainerapp/GoalsActivity.java
@@ -0,0 +1,13 @@
+package com.example.zdroa.trainerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GoalsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goals);
+    }
+}
