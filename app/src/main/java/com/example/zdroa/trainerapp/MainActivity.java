1f937989c296bc7ae3abb23f6ef526763e96ed9c
Daria Timofte <daria_ana99@yahoo.com>
2018-04-22 11:37:55 +0300

    Welcome Screen

--- app/src/main/java/com/example/zdroa/trainerapp/MainActivity.java
+++ app/src/main/java/com/example/zdroa/trainerapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.zdroa.trainerapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,7 +10,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        SharedPreferences sharedPref;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+         Boolean isInitialized = sharedPref.getBoolean("initialized",false);
+          if(!isInitialized)
+          {
+            Intent intent = new Intent(this, PersonalDataActivity.class);
+            startActivity(intent);
+          }
     }
 }
